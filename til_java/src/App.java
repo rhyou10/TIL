ded3ca0846b7d5f049e6605861e2845cc6cb5540
rhyou10 <rhyou10@nate.com>
2022-07-15 23:28:59 +0900

    vsc 기본 자바설정(scanner등)

--- til_java/src/App.java
+++ til_java/src/App.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a = 4;
+        System.out.println(a);
+        boolean b = true;
+        char ch = 'A';
+        String str = "ABC";
+
+        System.out.println(str);
+        System.out.println(ch);
+        System.out.println(b);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input_num = scanner.nextInt();
+        System.out.println(input_num);
+
+        scanner.close();
     }
 }
